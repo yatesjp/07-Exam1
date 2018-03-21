fce4169ea9014c743348193a3e67fc32b0571cac
yatesjp <yatesjp@rose-hulman.edu>
2018-03-20 21:35:07 -0400

    Why dd we have to wait? Preparation!

--- src/problem2.py
+++ src/problem2.py
@@ -2,8 +2,8 @@
 Exam 1, problem 2.
 
 Authors: David Mutchler, Dave Fisher, Matt Boutell, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.  March 2018.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         their colleagues and Jonah Yates.  March 2018.
+"""  # Done: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import rosegraphics as rg
 
@@ -115,10 +115,23 @@ def problem2(line1, line2, thickness, win):
       :type win:        rg.RoseWindow
     """
     # -------------------------------------------------------------------------
-    # TODO: 2. Implement and test this function, TESTING each step as you go.
+    # Done: 2. Implement and test this function, TESTING each step as you go.
     #          Tests have been written for you (above).
     #   See the IMPORTANT NOTE just above the DEF line above.
     # -------------------------------------------------------------------------
+
+    line1.attach_to(win)
+    line2.attach_to(win)
+
+    pacman = rg.Rectangle(line1.get_midpoint(), line2.get_midpoint())
+
+    pacman.outline_thickness = thickness
+
+    pacman.outline_color = line1.color
+
+    pacman.attach_to(win)
+
+    win.render()
 
 
 # -----------------------------------------------------------------------------
